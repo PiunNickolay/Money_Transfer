3d10dadab46004d536a9c84d7102636314730adf
PiunNickolay <147736400+PiunNickolay@users.noreply.github.com>
2024-05-31 19:37:48 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,8 @@
 package ru.netology
 
 fun main() {
-    println("Hello World!")
+    val amount = 50
+    val comission = 0.0075
+    var result = if (amount >= 35) amount*comission else 0
+    println("Комиссия составит: $result (рублей)")
 }
